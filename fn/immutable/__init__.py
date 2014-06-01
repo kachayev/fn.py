9843b40b694b5de83e05fc1dfdfb1e4046cd38b3
han semaj <sangho.nah@gmail.com>
2014-06-02 11:26:49 +1200

    Use alias to avoid naming conflict

--- fn/immutable/__init__.py
+++ fn/immutable/__init__.py
@@ -1,5 +1,5 @@
 from .heap import SkewHeap, PairingHeap
-from .list import LinkedList, Stack, Queue, Deque
+from .list import LinkedList, Stack, Queue, Deque as ListDeque
 from .trie import Vector
 from .finger import Deque
 #from .dict import Dict
