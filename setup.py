2084c307ce261cdd37f2e38af31e9c3b64c9409c
james sangho nah <sangho.nah@gmail.com>
2016-03-19 13:58:10 +1300

    Add missing versions to classifiers
    2.6 and 3.4

--- setup.py
+++ setup.py
@@ -35,10 +35,12 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ),
 )
